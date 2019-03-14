6bda9c76661cb10428bd38e5e9f960f2998f3a54
Zekkoo <lukasz212@gmail.com>
2019-03-14 21:05:38 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 // Global app controller
+import axios from "axios";
+
+
+const getResults = async query => {
+    const API_KEY = '394e7cb4d3f66b21b66fc6a1c6ff3371';
+    try{
+        const response = await axios.get(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${query}`);
+        const data = await response.data;
+        console.log(data.recipes);
+    } catch (error) {
+        alert(error);
+    }
+    
+    
+}
+
+getResults('brownies');
