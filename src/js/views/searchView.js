5f1e9e5ea6bd6b36aa53c903ef3e029056ec3780
Zekkoo <lukasz212@gmail.com>
2019-03-16 16:20:05 +0100

    added buttons

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,7 +8,9 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 }
+
 
 const limitRecipeTitle = (title, limit = 17) => {
     const newTitle = [];
